2492fd5e63a04b63a4a56fd18baaa7fb2e325080
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2019-06-10 15:16:35 +0200

    HACK: audio: add sound_trigger.stub.default
    This is to make the following error go away:
    E audiohalservice: Could not get passthrough implementation for
      android.hardware.soundtrigger@2.1::ISoundTriggerHw/default.
    E SoundTriggerHalImpl: couldn't load sound trigger module
      sound_trigger.primary (No such file or directory)
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- device.mk
+++ device.mk
@@ -34,6 +34,12 @@ PRODUCT_PACKAGES += audio.primary.i9305
 PRODUCT_PACKAGES += audio.primary.i9305_legacy
 PRODUCT_PACKAGES += audio.a2dp.default
 
+# Sound trigger
+PRODUCT_PACKAGES += \
+    sound_trigger.stub.default \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service \
+
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     device/samsung/i9305/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
